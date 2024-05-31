365996f5239a2127808bcf2750fb74c34b5097b4
milcho0604 <milcho0604@gmail.com>
2024-05-31 18:25:43 +0900

    DDL과 DML수정 후 테스트

--- SQL/DDL.sql
+++ SQL/DDL.sql
@@ -1,15 +1,11 @@
-use DOK;
-
--- 진료과 테이블 생성
 CREATE TABLE Departments (
     department_id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL,
-    description TEXT,
+    description VARCHAR(255),
     department_phone_number VARCHAR(15) NOT NULL UNIQUE,
     del_yn ENUM('N', 'Y') DEFAULT 'N'
 );
 
--- 환자 테이블 생성
 CREATE TABLE Patients (
     patient_id INT PRIMARY KEY AUTO_INCREMENT,
     patient_name VARCHAR(30) NOT NULL,
@@ -20,7 +16,6 @@ CREATE TABLE Patients (
     del_yn ENUM('N', 'Y') DEFAULT 'N'
 );
 
--- 관리자 테이블 생성
 CREATE TABLE Manager (
     manager_id INT PRIMARY KEY AUTO_INCREMENT,
     department_id INT,
@@ -29,7 +24,6 @@ CREATE TABLE Manager (
     FOREIGN KEY (department_id) REFERENCES Departments(department_id)
 );
 
--- 의사 테이블 생성
 CREATE TABLE Doctors (
     doctor_id INT PRIMARY KEY AUTO_INCREMENT,
     doctor_name VARCHAR(10) NOT NULL,
@@ -41,16 +35,15 @@ CREATE TABLE Doctors (
     FOREIGN KEY (department_id) REFERENCES Departments(department_id)
 );
 
--- 진료 기록 테이블 생성
 CREATE TABLE Medical_Records (
     record_id INT PRIMARY KEY AUTO_INCREMENT,
     patient_id INT NOT NULL,
     doctor_id INT NOT NULL,
     department_id INT NOT NULL,
     manager_id INT NOT NULL,
-    diagnosis TEXT NOT NULL,
+    diagnosis VARCHAR(255) NOT NULL,
     treatment TEXT NOT NULL,
-    prescription TEXT NOT NULL,
+    prescription VARCHAR(255) NOT NULL,
     visit_date DATETIME NOT NULL,
     del_yn ENUM('N', 'Y') DEFAULT 'N',
     FOREIGN KEY (patient_id) REFERENCES Patients(patient_id),
@@ -59,7 +52,6 @@ CREATE TABLE Medical_Records (
     FOREIGN KEY (department_id) REFERENCES Departments(department_id)
 );
 
--- 결제 테이블 생성
 CREATE TABLE Payments (
     record_id INT,
     amount FLOAT,
@@ -69,7 +61,6 @@ CREATE TABLE Payments (
     FOREIGN KEY (record_id) REFERENCES Medical_Records(record_id)
 );
 
--- 의사 일정 테이블 생성
 CREATE TABLE Schedules (
     doctor_id INT,
     vacation_date ENUM('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday') DEFAULT 'Sunday',
@@ -77,40 +68,45 @@ CREATE TABLE Schedules (
     FOREIGN KEY (doctor_id) REFERENCES Doctors(doctor_id)
 );
 
--- 피드백 테이블 생성
 CREATE TABLE Feedback (
     feedback_id INT PRIMARY KEY AUTO_INCREMENT,
     record_id INT NOT NULL,
     rating INT CHECK (rating > 0 AND rating <= 5),
-    feedback_text TEXT,
-    feedback_count TEXT NOT NULL,
+    feedback_text VARCHAR(255),
     del_yn ENUM('N', 'Y') DEFAULT 'N',
     FOREIGN KEY (record_id) REFERENCES Medical_Records(record_id)
 );
-ALTER TABLE Feedback
-DROP COLUMN feedback_count;
 
--- 접수 테이블 생성
 CREATE TABLE Registrations (
     registration_id INT PRIMARY KEY AUTO_INCREMENT,
-    patient_id INT,
-    doctor_id INT,
-    status ENUM('접수', '취소', '완료') DEFAULT '접수' NOT NULL,
-    created_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    manager_id INT NOT NULL,
-    FOREIGN KEY (patient_id) REFERENCES Patients(patient_id),
-    FOREIGN KEY (doctor_id) REFERENCES Doctors(doctor_id),
-    FOREIGN KEY (manager_id) REFERENCES Manager(manager_id)
+    doctor_id INT NOT NULL,
+    symptom VARCHAR(255),
+    created_time DATETIME,
+    FOREIGN KEY (doctor_id) REFERENCES Doctors(doctor_id)
 );
-ALTER TABLE Registrations
-ADD COLUMN symptoms VARCHAR(255);
 
--- 대기 테이블 생성
 CREATE TABLE Waiting (
     waiting_id INT PRIMARY KEY AUTO_INCREMENT,
     registration_id INT,
     patient_id INT,
     waiting_count INT,
+    status ENUM('접수', '취소', '완료') DEFAULT '접수' NOT NULL,
     FOREIGN KEY (registration_id) REFERENCES Registrations(registration_id),
     FOREIGN KEY (patient_id) REFERENCES Patients(patient_id)
 );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- SQL/DML.sql
+++ SQL/DML.sql
@@ -20,52 +20,86 @@ select * from Schedules;
 select * from Feedback;
 select * from Registrations;
 select * from Waiting;
-# De, Pa, Ma, Do, M_R, Pay, Se, Feed, Reg, Waiting
 
-INSERT INTO Departments(name, description, department_phone_number)
-VALUES ('내과', NULL, '02-1234-1234');
-INSERT INTO Departments(name, description, department_phone_number)
-VALUES ('이비인후과', '코, 귀 전문가', '02-1234-7777');
-INSERT INTO Departments(name, description, department_phone_number)
-VALUES ('정형외과','뼈는 내꺼', '02-8821-9330');
-
-INSERT INTO Patients(patient_name, identity_number, patient_phone, address) VALUES ('김지은', '000810-3217647', '010-2677-9981', '신대방 제2동');
-INSERT INTO Patients(patient_name, identity_number, patient_phone, address) VALUES ('박수현', '041102-4212553', '010-9942-2384', '도곡 1동');
-
-insert into Manager(department_id, manager_name) values (1, '김창현');
-insert into Manager(department_id, manager_name) values (2, '김창현');
-
-insert into Doctors(doctor_name, department_id, phone, email, office_number)
-values('홍문익', '1', '010-7331-9981', 'hong@naver.com', '1' );
-insert into Doctors(doctor_name, department_id, phone, email, office_number)
-values('김창식', '2', '010-8881-1234', 'goose@naver.com', '2' );
-
-insert into Medical_Records(patient_id, doctor_id, department_id, manager_id, diagnosis, treatment, prescription, visit_date)
-VALUES('1', '2', '2','1', '비염', '콧물 제거', '물약', '2024-01-31' );
-insert into Medical_Records(patient_id, doctor_id, department_id, manager_id, diagnosis, treatment, prescription, visit_date)
-VALUES('1', '2', '2','1', '중이염', '레이저 치료', '알약', '2024-02-12' );
-
-insert into Payments(record_id, amount, payment_date, payment_method) VALUES ('1', '4800', '2024-01-31', '오프라인');
-insert into Payments(record_id, amount, payment_date, payment_method) VALUES ('2', '8800', '2024-02-12', '온라인'); # 여태까지 결제한 이력을 보는 용도로
-
-insert into Schedules(doctor_id, vacation_date) VALUES ('1', 'Sunday');
-insert into Schedules(doctor_id, vacation_date) VALUES ('2', 'wednesday');
-insert into Schedules(doctor_id, vacation_date) VALUES ('2', 'Friday');
-
-insert into Feedback(record_id, rating, feedback_text)
-values('1', 5, '무뚝뚝하세요');
-insert into Feedback(record_id, rating, feedback_text)
-values('2', 4, '아프게 치료해요');
-
-insert into Registrations(patient_id,doctor_id, status, manager_id, symptoms) # status는 어떻게 정해? 접수는 여기서 정하는? 일단 테스트만, 테이블 분리 필요해보임
-values ('1','2','완료', '1', '중이염');
-insert into Registrations(doctor_id, status, manager_id, symptoms) # status는 어떻게 정해? 접수는 여기서 정하는? 일단 테스트만, 테이블 분리 필요해보임
-values ('2','접수', '1', '중이염');
-
-insert into Waiting(registration_id, patient_id) VALUES ('2', '2' );
-insert into Waiting(registration_id, patient_id) VALUES ('2', '2' );
+-- De, Pa, Ma, Do, M_R, Pay, Se, Feed, Reg, Waiting 
 
 
+
+INSERT INTO Departments (name, description, department_phone_number)
+VALUES ('내과', '내과 진료과', '02-1234-1234'),
+       ('소아과', '소아과 진료과', '02-5678-5678');
+
+INSERT INTO Patients (patient_name, identity_number, patient_phone, address)
+VALUES ('홍길동', '900101-1234567', '010-1234-5678', '서울시 강남구'),
+       ('김영희', '950202-2345678', '010-2345-6789', '서울시 서초구');
+
+INSERT INTO Manager (department_id, manager_name)
+VALUES
+    (1, '관리자1'),
+    (2, '관리자2');
+
+INSERT INTO Doctors (doctor_name, department_id, phone, email, office_number)
+VALUES
+    ('김지은', 1, '010-3456-7890', 'kimcs@hospital.com', '101'),
+    ('박수현', 2, '010-4567-8901', 'leeyh@hospital.com', '102');
+INSERT INTO Doctors (doctor_name, department_id, phone, email, office_number)
+VALUES('김창현', 2, '010-7755-6298', 'kch@naver.com','103');
+ INSERT INTO Registrations (doctor_id, symptom)
+SELECT d.doctor_id, '새로운 증상'  -- 실제로 사용할 증상 값으로 변경해야 합니다.
+FROM Doctors AS d
+JOIN Schedules AS s ON d.doctor_id = s.doctor_id
+WHERE s.vacation_date != DAYNAME(NOW());
+
+INSERT INTO Registrations (doctor_id, symptom)
+VALUES
+    (1, '감기'),
+    (2, '독감');
+
+INSERT INTO Registrations (doctor_id, symptom)
+VALUES
+    (1, '감기'),
+    (2, '독감');
+
+describe Waiting;
+
+INSERT INTO Waiting (registration_id, patient_id, waiting_count, created_time)
+VALUES
+    (1, 1, 1, NOW());
+INSERT INTO Waiting (registration_id, patient_id, waiting_count, created_time, status)
+VALUES
+    (2, 2, 2, NOW(), '접수');
+                            -- 환자        의사          진료과         관리자         진단      치료          처방
+INSERT INTO Medical_Records (patient_id, doctor_id, department_id, manager_id, diagnosis, treatment, prescription, visit_date)
+VALUES
+    (1, 1, 1, 1, '감기', '약물 치료', '타이레놀', CURDATE()),
+    (2, 2, 2, 2, '독감', '약물 치료', '타미플루', CURDATE());
+select * from Medical_Records;
+
+INSERT INTO Payments (record_id, amount, payment_date, payment_method, del_yn)
+VALUES
+    (1, 50000, NOW(), '온라인', 'N'),
+    (2, 70000, NOW(), '오프라인', 'N');
+
+INSERT INTO Schedules (doctor_id, vacation_date, del_yn)
+VALUES
+    (1, 'Sunday', 'N'),
+    (2, 'Saturday', 'N');
+select * from Doctors;
+
+INSERT INTO Schedules (doctor_id, vacation_date, del_yn)
+VALUES (3, 'Friday','N');
+
+INSERT INTO Feedback (record_id, rating, feedback_text, del_yn)
+VALUES
+    (1, 5, '친절한 진료 감사합니다.', 'N'),
+    (2, 4, '진료가 만족스러웠습니다.', 'N');
+
+-- insert문으로 데이터 삽입, 의사의 휴무일과 오늘의 날짜가 다른 경우에만 삽입 
+ INSERT INTO Registrations (doctor_id, symptom)
+SELECT 3, '감기'
+FROM Doctors AS d
+JOIN Schedules AS s ON d.doctor_id = s.doctor_id
+WHERE d.doctor_id = docor_id && s.vacation_date != DAYNAME(NOW());
 
 
 SELECT
