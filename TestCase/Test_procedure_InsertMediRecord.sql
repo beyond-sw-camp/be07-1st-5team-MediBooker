ba2d50a2aec065957ea3fab8729d523a8974b6ab
milcho0604 <milcho0604@gmail.com>
2024-06-03 15:45:10 +0900

    수정

--- TestCase/Test_procedure_InsertMediRecord.sql
+++ TestCase/Test_procedure_InsertMediRecord.sql
@@ -23,9 +23,19 @@ CALL InsertMediRecord (
 
 -- 테스트 3 : 진료기록 추가 및 대기 데이터 삭제
 CALL InsertMediRecord (
-    13, -- 대기 목록에 존재하지 않는 patient_id (환자ID)
-    1, -- 대기 목록에 존재하지 않는 doctor_id_param (의사 ID)
+    13, -- 대기 목록에 존재하는 patient_id (환자ID)
+    1, -- 대기 목록에 존재하는 doctor_id_param (의사 ID)
     '콧물 질질 줄줄', -- diagnosis_param (진단 내용)
     '콧물 다 빼버려', -- treatment_param (치료 내용)
     '파란 물약' -- prescription_param (처방 내용)
 );
+
+-- 테스트 4 : 진료기록 추가 및 대기 데이터 삭제 + 결제 서비스 추가
+CALL InsertMediRecord(
+        3, -- 대기 목록에 존재하는 patient_id (환자ID)
+        4, -- 대기 목록에 존재하는 doctor_id_param (의사 ID)
+        '장염', -- diagnosis_param (진단 내용)
+        '엄마 손은 약손',-- treatment_param (치료 내용)
+        '빨간 물약', -- prescription_param (처방 내용)
+        50000, -- 청구할 금액
+        '오프라인'); -- 결제 방법
